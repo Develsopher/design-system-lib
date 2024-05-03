7fda46eccae49a7d6e8ab03eda17f77b0ba99b31
junhyeongchae <jchae7184@gmail.com>
2024-05-03 18:51:11 +0900

    storybook setting

---
+++ services/storybook/stories/ReactComponents/Layout/Block.stories.tsx
@@ -0,0 +1,13 @@
+import "@develsopher/react-components-layout/style.css";
+import { Block as _Block } from "@develsopher/react-components-layout";
+
+export default {
+  title: "React Components/Layout/Block",
+  component: _Block,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["authors"],
+};
+
+export const BlockStory = {};
